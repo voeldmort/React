39d675882c1c180e370c28090cebe003459d8b60
voeldmort <ranavijaysuryavanshi@yahoo.com>
2023-11-04 12:45:39 +0530

    Edit in template

--- reactrouter/src/components/Footer/footer.jsx
+++ reactrouter/src/components/Footer/footer.jsx
@@ -74,10 +74,10 @@ export default function Footer() {
                 <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
-                        © 2023
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
+                        © 2023 Ranavijay Singh
+                        {/* <a href="https://hiteshchoudhary.com/" className="hover:underline">
                             hiteshchoudhary
-                        </a>
+                        </a> */}
                         . All Rights Reserved.
                     </span>
                     <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
